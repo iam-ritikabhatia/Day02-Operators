b73f2de5fecaaa67dae152cd4539196c93d6a136
Ritika Bhatia <139344947+iam-ritikabhatia@users.noreply.github.com>
2024-07-17 12:34:21 +0530

    Add files via upload

---
+++ Day2.js
@@ -0,0 +1,178 @@
+// Add 2 number
+
+
+ let addA = 20;
+let addB = 30;
+console.log(addA + addB);
+
+
+
+// Subtract 2 number
+
+
+let subtractC = 80;
+let subtractD = 10;
+console.log(subtractC - subtractD);
+
+
+//  Multiply 2 number
+
+
+let multiplyE = 5;
+let multiplyF = 6;
+console.log(multiplyE * multiplyF);
+
+
+// Divide 2 Number
+
+
+let divideG = 8;
+let divideH = 2;
+console.log(divideG / divideH);
+
+
+// Remainder when one number is divided by another
+
+let remainderI = 5;
+let remainderJ = 6;
+console.log(remainderJ % remainderI);;
+
+
+// += operator
+
+let myNumber = 5;
+myNumber += 10;
+console.log(myNumber);
+
+
+// -= operator
+
+let subtractAssignment = 90;
+subtractAssignment -= 50;
+console.log(subtractAssignment);
+
+
+// Comparison
+
+console.log( 8 > 1);
+console.log( 1 < 2);
+console.log( 5 >= 2);
+console.log( 7 <= 6);
+console.log( 5 == 2);
+console.log( 8 === 5);
+
+
+
+//  && operator
+
+let conditionOne = false;
+let conditionTwo = true;
+console.log(conditionOne && conditionTwo);
+
+// 
+
+let statementOne = true;
+let statementTwo = true;
+console.log(statementOne && statementTwo);
+
+
+// 
+
+let herWords = false;
+let hisWords = false;
+console.log(hisWords && herWords);
+
+
+
+
+
+// || operator
+
+let myWords = false;
+myWords = true;
+let motherWords = false;
+motherWords = true;
+let res =  myWords ||  motherWords; 
+console.log(res); 
+
+// In || operator either one condition is true the output is true.  
+
+
+// ! operator
+
+
+let condition = true;
+let negatedCondition = !condition;
+console.log(negatedCondition);
+
+
+// 
+
+let Mywords = false;
+let neglectedCondition = !Mywords;
+console.log(neglectedCondition);
+
+/* Note : condition is true then negatedCondition is false & condition is false then negatedCondition is true.*/
+
+
+
+// Ternary Operator (?)
+
+let number = -5;
+let result = (number >= 0) ? "Positive" :  "Negative";
+console.log(result);
+
+
+
+// Script Features request
+
+let number1 = 5;
+let number2 = 10;
+
+let addition = number1 + number2;
+let subtraction = number1 - number2;
+let multipliaction = number1 * number2;
+let division = number1 / number2;
+let remainder = number1 % number2;
+console.log('addition' + addition);
+console.log('subtraction' + subtraction);
+console.log('multiplication' + multipliaction);
+console.log('division' + division);
+console.log('remainder'  + remainder);
+
+
+let greaterThan = number1 > number2;
+let lessThan = number1 < number2;
+let assignment = number1 == number2;
+let lessThanEqualTo = number1 <= number2;
+let greaterThanEqualTo = number1 >= number2;
+let strictNotEqualTo = number1 != number2;
+
+console.log('greaterThan' + greaterThan);
+console.log('lessThan' + lessThan);
+console.log('assignment' + assignment);
+console.log('lessThanEqualTo' + lessThanEqualTo);
+console.log('greaterThanEqualTo' + greaterThanEqualTo);
+console.log('strictNotEqualTo' + strictNotEqualTo);
+
+
+let id = (number1 > number2) ? "Positive" : "Negative";
+console.log(id);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
